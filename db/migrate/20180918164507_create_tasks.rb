e70968b8898a6d3346b163c8c7831fa718557ef5
Matt Parlette <matthew.parlette@gmail.com>
2018-09-19 14:27:45 -0400

    Add tasks WIP

---
+++ db/migrate/20180918164507_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.date :date, null: true, default: nil
+      t.int :mark, default: 0
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
