afd7786e77d374b57b512a495eef19d9c73897bf
Matthew Parlette <matthew.parlette@gmail.com>
2018-09-21 16:54:43 -0400

    Add cancelled mark for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   belongs_to :category
-  enum mark: { not_marked: 0, rescheduled: 1, progressed: 2, done: 3 }
+  enum mark: { not_marked: 0, rescheduled: 1, progressed: 2, done: 3, cancelled: 4 }
 end
